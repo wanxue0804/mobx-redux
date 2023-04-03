e20cbb0ed3b1a1f9cd6d00e94dcc70c336f3f16b
snail d <937527543@qq.com>
2023-04-03 02:50:45 +0000

    补充redux写法

--- src/models/redux/store.ts
+++ src/models/redux/store.ts
@@ -5,8 +5,13 @@ import reducers from '@/redux/reducers';
 
 // 增强器
 const storeEnhancer: StoreEnhancer = applyMiddleware(thunk);
-const storeEnhancerStoreCreate: StoreEnhancerStoreCreator = storeEnhancer(createStore);
+// const storeEnhancerStoreCreate: StoreEnhancerStoreCreator = storeEnhancer(createStore);
 
 
-const store: Store = storeEnhancerStoreCreate(reducers);
+// const store: Store = storeEnhancerStoreCreate(reducers);
+
+// 跟上面分开写是一样的
+const store = createStore(reducers, applyMiddleware(thunk));
+
+
 export default store;
